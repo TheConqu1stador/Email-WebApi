f395284daf32a070fb50a113306775f4f7e665fa
TheConqu1stador <43194859+TheConqu1stador@users.noreply.github.com>
2022-02-09 23:35:54 +0300

    add comments

--- testTask/testTask/Models/Mail.cs
+++ testTask/testTask/Models/Mail.cs
@@ -2,17 +2,39 @@ namespace testTask.Models
 {
     public class Mail
     {
+        /// <summary>
+        /// ID сообщения
+        /// </summary>
         public long Id { get; set; }
+
+        /// <summary>
+        /// Тема сообщения
+        /// </summary>
         public string Subject { get; set; }
 
+        /// <summary>
+        /// Тело сообщения
+        /// </summary>
         public string Body { get; set; }
 
+        /// <summary>
+        /// Получатель сообщения
+        /// </summary>
         public string Recipient { get; set; }
 
+        /// <summary>
+        /// Дата отправки
+        /// </summary>
         public DateTime SendDate { get; set; }
 
+        /// <summary>
+        /// Результат отправки (OK | Failed)
+        /// </summary>
         public string Result { get; set; }
 
+        /// <summary>
+        /// Сообщение об ошибке
+        /// </summary>
         public string? FailedMessage { get; set; }
     }
 }
--- testTask/testTask/Models/PostSendMail.cs
+++ testTask/testTask/Models/PostSendMail.cs
@@ -2,10 +2,19 @@ namespace testTask.Models
 {
     public class PostSendMail
     {
+        /// <summary>
+        /// Тема сообщения
+        /// </summary>
         public string Subject { get; set; }
 
+        /// <summary>
+        /// Тело сообщения
+        /// </summary>
         public string Body { get; set; }
 
+        /// <summary>
+        /// Получатели сообщения
+        /// </summary>
         public IReadOnlyCollection<string> Recipients { get; set; }
     }
 }
